1f2e0708cfe7c56fc0d97e35cb8876c25cea2d92
Naqi <Hurricaneirene07#>
2021-03-11 19:52:13 -0500

    First Commit: Made models and a template for forum

--- forum/models.py
+++ forum/models.py
@@ -1,3 +1,27 @@
-from django.db import models
-
 # Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Board(models.Model):
+    name = models.CharField(max_length=30, unique=True)
+    description = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.name
+
+
+class Topic(models.Model):
+    subject = models.CharField(max_length=500)
+    last_updated = models.DateField(auto_now_add=True)
+    board = models.ForeignKey(Board, related_name='topics', on_delete=models.PROTECT)
+    starter = models.ForeignKey(User, related_name='topics', null=True, on_delete=models.SET_NULL)
+
+
+class Post(models.Model):
+    message = models.TextField(max_length=3000)
+    topic = models.ForeignKey(Topic, related_name='posts', on_delete=models.PROTECT)
+    created_when = models.DateField(auto_now_add=True)
+    created_by = models.ForeignKey(User, related_name='posts', null=True, on_delete=models.SET_NULL)
+    updated_when = models.DateField(null=True)
+    updated_by = models.ForeignKey(User, null=True, related_name='+', on_delete=models.SET_NULL)
--- forum/views.py
+++ forum/views.py
@@ -1,3 +1,13 @@
+# Views are Python functions that receive an HttpRequest object
+# and returns an HttpResponse object. Receive a request as a parameter
+# and returns a response as a result.
+
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Board
+
 
 # Create your views here.
+def render_forum(request):
+    boards = Board.objects.all()
+    return render(request, 'forum.html', {'boards': boards})
